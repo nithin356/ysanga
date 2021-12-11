7ad4102f02481cc53845fbd16755f1f4d0558db6
nithin356 <nithinbhandary356@gmail.com>
2021-12-11 12:06:45 +0530

    update

--- backend/customer/verifyotp/index.php
+++ backend/customer/verifyotp/index.php
@@ -6,7 +6,7 @@ include_once '../../access/connect.php';
 session_start();
 $phone = $_POST['phone'];
 $otp = $_POST['otp'];
-$QueryVerifyOTP = mysqli_query($connection, "SELECT * FROM yn_user WHERE yn_phone='$phone'");
+$QueryVerifyOTP = mysqli_query($connection, "SELECT * FROM ys_user WHERE yn_phone='$phone'");
 $count = mysqli_num_rows($QueryVerifyOTP);
 if ($count == 1) {
     $row = mysqli_fetch_assoc($QueryVerifyOTP);
@@ -14,7 +14,7 @@ if ($count == 1) {
     $uid = $row['yn_uid'];
     if ($otp == $checkotp) {
         //DELETE AFTER VERIFYING
-        $deleteVerifyOTP = mysqli_query($connection, "UPDATE yn_user set yn_otp='' WHERE yn_phone = '$phone'");
+        $deleteVerifyOTP = mysqli_query($connection, "UPDATE ys_user set yn_otp='' WHERE yn_phone = '$phone'");
         $_SESSION['yn_uid'] = $uid;
         $data = array("status" => "OK", "message" => "success");
     } else {

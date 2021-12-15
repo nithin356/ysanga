5fbe309b9e734c4e1389303fc422f4076f22f289
nithin356 <nithinbhandary356@gmail.com>
2021-12-15 21:16:47 +0530

    updated

--- js/api_url.js
+++ js/api_url.js
@@ -1,6 +1,6 @@
 const API_URL = "";
 $(document).ready(function () {
-  if (window.location.href.indexOf("franky") > -1) {
+  if (window.location.href.indexOf("yuvaksangabackend") > -1) {
     API_URL =
       "http://yuvaksangabackend-env.eba-p5tp78ge.us-west-2.elasticbeanstalk.com/backend/";
   } else {
